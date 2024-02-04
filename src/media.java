46898dd7f7964171ab3bd2180da2a61042849d6a
Istan <thiago.sh26@gmail.com>
2024-02-04 16:22:36 -0300

    Realizando a atividade

--- src/media.java
+++ src/media.java
@@ -1,4 +1,27 @@
 
 public class media {
+    public static void main(String[] args) {
+        double nota1, nota2, nota3, nota4, media;
 
+        // Leitura das notas
+        System.out.println("digite a primeira nota: ");
+        nota1 = Double.parseDouble(System.console().readLine());
+
+        System.out.println("digite a segunda nota: ");
+        nota2 = Double.parseDouble(System.console().readLine());
+
+        System.out.println("digite a terceira nota: ");
+        nota3 = Double.parseDouble(System.console().readLine());
+
+        System.out.println("digite a quarta nota: ");
+        nota4 = Double.parseDouble(System.console().readLine());
+
+        // Calculo da media
+
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        // Impressao da media
+        System.out.println("A media do aluno é: " + media);
+
+    }
 }
