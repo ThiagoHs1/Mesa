01c844637993e942dc5a6da442b277a87e49ec47
Istan <thiago.sh26@gmail.com>
2024-02-27 20:16:30 -0300

    first commit

--- src/com/anotation/App.java
+++ src/com/anotation/App.java
@@ -5,9 +5,14 @@ import com.anotation.thiagohs.modelo.Usuario;
 
 public class App {
     public static void main(String[] args) {
+        // Verifica se a classe Usuario tem a anotação Tabela
         if (Usuario.class.isAnnotationPresent(Tabela.class)) {
+            // Recupera a anotação Tabela da classe Usuario
             Tabela tabela = Usuario.class.getAnnotation(Tabela.class);
+            // Imprime o nome da tabela
             System.out.println("Nome da tabela: " + tabela.nome());
+        } else {
+            System.out.println("A anotação 'Tabela' não está presente na classe 'Usuario'.");
         }
     }
 }
--- src/com/anotation/thiagohs/modelo/Usuario.java
+++ src/com/anotation/thiagohs/modelo/Usuario.java
@@ -6,5 +6,21 @@ import com.anotation.thiagohs.anotacoes.Tabela;
 public class Usuario {
   private int id;
   private String nome;
-  // Getters e setters...
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
 }
