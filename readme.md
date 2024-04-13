b34e0889542a40c112132ec5fbd6602df1f189a4
Istan <thiago.sh26@gmail.com>
2024-04-13 15:55:05 -0300

    Criando a classe produto e seus dao, interface e testes

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Projeto simples para treinar o uso de hibernate, criado uma classe produto para utilizar o mapeamento do JPA
+
+
+
+## ATIVIDADE REALIZADA DURANTE O CURSO PROFISSÃO: DESENVOLVEDOR FULL STACK JAVA
---
+++ src/main/java/br/com/rpires/domain/Produto.java
@@ -0,0 +1,80 @@
+package br.com.rpires.domain;
+
+import org.hibernate.annotations.Check;
+
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUTO_APOSTILA")
+public class Produto {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "produto_seq")
+  @SequenceGenerator(name = "produto_seq", sequenceName = "produto_seq", initialValue = 1, allocationSize = 1)
+  private Long id;
+
+  @Column(name = "CODIGO", length = 5, nullable = false, unique = true)
+  private String codigo;
+
+  @Column(name = "DESCRICAO", length = 100, nullable = true)
+  private String descricao;
+
+  @ManyToOne
+  @JoinColumn(name = "id_curso_fk", foreignKey = @ForeignKey(name = "fk_curso_produto_apostila"), referencedColumnName = "id", nullable = false)
+  private Curso curso;
+
+  @ManyToOne
+  @JoinColumn(name = "id_matricula_fk", foreignKey = @ForeignKey(name = "fk_matricula_produto_apostila"), referencedColumnName = "id", nullable = false)
+  private Matricula matricula;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getCodigo() {
+    return codigo;
+  }
+
+  public void setCodigo(String codigo) {
+    this.codigo = codigo;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public Curso getCurso() {
+    return curso;
+  }
+
+  public void setCurso(Curso curso) {
+    this.curso = curso;
+  }
+
+  public Matricula getMatricula() {
+    return matricula;
+  }
+
+  public void setMatricula(Matricula matricula) {
+    this.matricula = matricula;
+  }
+}
